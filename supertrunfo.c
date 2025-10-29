34cb7addb230c3c667d30487e3bc38c9c2c59eb1
lucasthecrea-tor <lucas.andrade2597@gmail.com>
2025-10-29 09:18:08 -0300

    Desafio Cartas Super Trunfo

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+int main(){
+  char estado[50] = "PR";
+  int codigo = 41;
+  char cidade[50] = "CURITIBA";
+  int populacao = 1773718;
+  double pib = 98000000000.00;
+  double area = 435.036;  
+  int pontos_turisticos = 34;
+  double densidade_populacional = 4209.8;
+  double pib_per_capita = 45318.46;
+
+
+  estado = "SP";
+  codigo = 11;
+  cidade = "SAO PAULO";
+  populacao = 11890000;
+  pib = 3500000000000.00;
+  area = 1521;
+  pontos_turisticos = 40;
+  densidade_populacional = 7527.8;
+  pib_per_capita = 60805.18;
+
+  estado = "RJ";
+  codigo = 21;
+  cidade = "RIO DE JANEIRO";
+  populacao = 6720000;
+  pib = 331280000000.00;
+  area = 1200;
+  pontos_turisticos = 58;
+  densidade_populacional = 5556;
+  pib_per_capita = 53078.23;
+
+  estado = "DF";
+  codigo = 61;
+  cidade = "DISTRITO FEDERAL";
+  populacao = 2980000;
+  pib = 286900000000.00;
+  area = 5761;
+  pontos_turisticos = 32;
+  densidade_populacional = 489.1;
+  pib_per_capita = 116713.39;
+
+
+}
